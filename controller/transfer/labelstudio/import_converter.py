757fe8195c5a2ed1c9c61ed8f214752e84646e5a
SirDeGraf <simon.degraf@icloud.com>
2022-11-10 14:30:56 +0100

    Makes changes to converter logic

--- controller/transfer/labelstudio/import_converter.py
+++ controller/transfer/labelstudio/import_converter.py
@@ -7,45 +7,55 @@ def manage_converting_data(project_id: str, task_id: str) -> None:
     task = upload_task.get(project_id, task_id)
     file_path = download_file(project_id, task)
     mappings = json.loads(task.mappings)
-    user_mapping = mappings.get("user")
+    user_mapping = mappings.get("users")
     attribute_task_mapping = mappings.get("tasks")
 
-    with open(file_path, "w+") as file:
-        if task.file_type == "JSON":
-            data = json.load(file)
-            converted_data = __extract_and_compose_data(data, user_mapping, attribute_task_mapping)
-            file.write(json.dumps(converted_data))
-            print("path", file_path)
+    converted_data = None
+    with open(file_path) as file:
+        data = json.load(file)
+        converted_data = __extract_and_compose_data(
+            data, user_mapping, attribute_task_mapping
+        )
+    print("converted_data", converted_data)
 
+    with open("converted_file.json", "w") as file:
+        pass
 
 def __extract_and_compose_data(data, user_mapping, attribute_task_mapping):
+
+    print("user_mapping", user_mapping)
+    print("attribute_task_mapping", attribute_task_mapping)
+
     records = []
     for record_item in data:
         record = {
-            "label_studio_id": record_item.get("id"),
-            "data": record_item.get("data"),
+            "label_studio_id": record_item.get("id")
         }
 
         for attribute in record_item.get("data").keys():
             record[attribute] = record_item.get("data").get(attribute)
         for annotation_item in record_item.get("annotations"):
-            if annotation_item.get("type") != "choices":
-                continue
+            for result in annotation_item["result"]:
+                if result.get("type") != "choices":
+                    continue
 
-            task_name = annotation_item.get("from_name")
-            table_name_label = f"__{task_name}__MANUAL"
+                task_name = result.get("from_name")
+                table_name_label = f"__{task_name}__MANUAL"
 
-            if attribute_task_mapping.get(task_name) == "ATTRIBUTE_SPECIFIC": # TODO ENUMS
-                table_name_label = f"{attribute_task_mapping.get(task_name)}{table_name_label}"
+                if (
+                    attribute_task_mapping.get(task_name) == "ATTRIBUTE_SPECIFIC"
+                ):  # TODO ENUMS
+                    table_name_label = (
+                        f"{attribute_task_mapping.get(task_name)}{table_name_label}"
+                    )
 
-            table_name_created_by = f"{table_name_label}__CREATED_BY"
+                table_name_created_by = f"{table_name_label}__CREATED_BY"
 
-            record[table_name_label] = annotation_item.get("value").get("choices")
-            record[table_name_created_by] = user_mapping.get(
-                annotation_item.get("completed_by")
-            )
+                record[table_name_label] = result.get("value").get("choices")[0]
+                record[table_name_created_by] = user_mapping.get(
+                    str(annotation_item.get("completed_by"))
+                )
 
         records.append(record)
 
     return records
-
