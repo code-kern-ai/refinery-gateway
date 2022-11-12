b6e1c0d11c4f34aaf82c038f5b57f911f2e62180
Johannes Hötter <johannes.hoetter@kern.ai>
2022-11-12 14:20:27 +0100

    adds try finally

--- controller/record/manager.py
+++ controller/record/manager.py
@@ -118,58 +118,61 @@ def predict_one(
 
     project = project_manager.get_project(project_id)
     record = create_record(project_id, record_data)
-    found_embedding = False  # only for the beta version
-    for embedding in project.embeddings:
-        if embedding.is_selected_for_inference:  # only for the beta version
-            embedding_manager.create_single_embedding(
+    try:
+        found_embedding = False  # only for the beta version
+        for embedding in project.embeddings:
+            if embedding.is_selected_for_inference:  # only for the beta version
+                embedding_manager.create_single_embedding(
+                    project_id, embedding.id, record.id
+                )
+                found_embedding = True
+
+        if not found_embedding:
+            raise Exception("Embedding not found")
+
+        results = []
+        for labeling_task in project.labeling_tasks:
+            prediction_task = None
+            for information_source in labeling_task.information_sources:
+                if (
+                    information_source.is_selected_for_inference
+                ):  # only for the beta version
+                    if (
+                        information_source.type
+                        == enums.InformationSourceType.ACTIVE_LEARNING.value
+                    ):
+                        labels = payload_manager.get_active_learning_on_1_record(
+                            project_id, str(information_source.id), str(record.id)
+                        )
+                        if len(labels) > 0:
+                            labels = labels[str(record.id)]
+                            prediction_task = {
+                                "label": labels[1],
+                                "confidence": labels[0],
+                            }
+
+                    elif (
+                        information_source.type
+                        == enums.InformationSourceType.LABELING_FUNCTION.value
+                    ):
+                        pass
+                    elif (
+                        information_source.type
+                        == enums.InformationSourceType.ZERO_SHOT.value
+                    ):
+                        pass
+            if prediction_task is not None:
+                results.append(
+                    {
+                        "labeling_task": labeling_task.name,
+                        "prediction": prediction_task,
+                    }
+                )
+
+        if found_embedding:
+            embedding_manager.delete_single_embedding(
                 project_id, embedding.id, record.id
             )
-            found_embedding = True
-
-    if not found_embedding:
-        raise Exception("Embedding not found")
-
-    results = []
-    for labeling_task in project.labeling_tasks:
-        prediction_task = None
-        for information_source in labeling_task.information_sources:
-            if (
-                information_source.is_selected_for_inference
-            ):  # only for the beta version
-                if (
-                    information_source.type
-                    == enums.InformationSourceType.ACTIVE_LEARNING.value
-                ):
-                    labels = payload_manager.get_active_learning_on_1_record(
-                        project_id, str(information_source.id), str(record.id)
-                    )
-                    if len(labels) > 0:
-                        labels = labels[str(record.id)]
-                        prediction_task = {
-                            "label": labels[1],
-                            "confidence": labels[0],
-                        }
-
-                elif (
-                    information_source.type
-                    == enums.InformationSourceType.LABELING_FUNCTION.value
-                ):
-                    pass
-                elif (
-                    information_source.type
-                    == enums.InformationSourceType.ZERO_SHOT.value
-                ):
-                    pass
-        if prediction_task is not None:
-            results.append(
-                {
-                    "labeling_task": labeling_task.name,
-                    "prediction": prediction_task,
-                }
-            )
-
-    if found_embedding:
-        embedding_manager.delete_single_embedding(project_id, embedding.id, record.id)
-
-    delete_record(project_id, record.id)
+    finally:
+        delete_record(project_id, record.id)
     return {"record": record_data, "predictions": results}
