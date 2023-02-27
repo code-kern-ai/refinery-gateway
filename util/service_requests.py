f31ae7dc68b38d277ced54bd6bb3378450c35e94
JWittmeyer <jens.wittmeyer@onetask.ai>
2023-02-27 10:56:06 +0100

    Changes response unpacking to single value

--- util/service_requests.py
+++ util/service_requests.py
@@ -6,22 +6,21 @@ from graphql import GraphQLError
 
 def post_call_or_raise(url: str, data: Dict[str, Any]) -> Any:
     response = requests.post(url, json=data)
-    if response.status_code == 200:
-        result, _ = response.json()
-        return result
-    else:
+
+    if response.status_code != 200:
         raise GraphQLError(response.text)
+
+    return response.json()
 
 
 def get_call_or_raise(url: str, params: Dict = None) -> Any:
     if params is None:
         params = {}
     response = requests.get(url=url, params=params)
-    if response.status_code == 200:
-        result, _ = response.json()
-        return result
-    else:
+    if response.status_code != 200:
         raise GraphQLError(response.text)
+
+    return response.json()
 
 
 def delete_call_or_raise(url: str, params: Dict = None) -> int:
