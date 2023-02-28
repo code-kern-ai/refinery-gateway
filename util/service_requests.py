ada85c3ed2eb57e80c673c08cfe43b6cf6ce6501
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2023-02-28 09:42:34 +0100

    Fast Api JSONResponst change (#118)
    * Changes response unpacking to single value
    
    * Adds config change
    
    ---------
    
    Co-authored-by: JWittmeyer <jens.wittmeyer@onetask.ai>

--- util/service_requests.py
+++ util/service_requests.py
@@ -6,22 +6,27 @@ from graphql import GraphQLError
 
 def post_call_or_raise(url: str, data: Dict[str, Any]) -> Any:
     response = requests.post(url, json=data)
-    if response.status_code == 200:
-        result, _ = response.json()
-        return result
-    else:
+
+    if response.status_code != 200:
         raise GraphQLError(response.text)
+
+    if response.headers.get("content-type") == "application/json":
+        return response.json()
+    else:
+        return response.text
 
 
 def get_call_or_raise(url: str, params: Dict = None) -> Any:
     if params is None:
         params = {}
     response = requests.get(url=url, params=params)
-    if response.status_code == 200:
-        result, _ = response.json()
-        return result
-    else:
+    if response.status_code != 200:
         raise GraphQLError(response.text)
+
+    if response.headers.get("content-type") == "application/json":
+        return response.json()
+    else:
+        return response.text
 
 
 def delete_call_or_raise(url: str, params: Dict = None) -> int:
