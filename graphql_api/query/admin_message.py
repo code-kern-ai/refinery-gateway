ab41d9473cd3bc74ca336e2e468b75790f4797bb
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-02 10:29:32 +0100

    Small fix

--- graphql_api/query/admin_message.py
+++ graphql_api/query/admin_message.py
@@ -17,7 +17,7 @@ class AdminMessageQuery(graphene.ObjectType):
     def resolve_all_admin_messages(self, info, limit: int = 100) -> AdminMessage:
         auth.check_demo_access(info)
         auth.check_admin_access(info)
-        return manager.get_messages(limit)
+        return manager.get_messages(limit, active_only=False)
 
     def resolve_all_active_admin_messages(self, info, limit: int = 100) -> AdminMessage:
         auth.check_demo_access(info)
