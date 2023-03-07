506f8d3f54c753a33b1f4b570e75e5e1e2c6d945
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-07 11:12:58 +0100

    Extends healthcheck with database logic

--- api/healthcheck.py
+++ api/healthcheck.py
@@ -1,7 +1,22 @@
 from starlette.endpoints import HTTPEndpoint
 from starlette.responses import PlainTextResponse
+from submodules.model.business_objects import general
 
 
 class Healthcheck(HTTPEndpoint):
     def get(self, request) -> PlainTextResponse:
-        return PlainTextResponse("OK")
+        error_message = ""
+
+        if not __test_database_connection():
+            error_message += " database_conncetion "
+
+        text_response = error_message if error_message else "OK"
+        return PlainTextResponse(text_response)
+
+
+def __test_database_connection() -> bool:
+    try:
+        general.execute("SELECT 1")
+        return True
+    except Exception:
+        return False
