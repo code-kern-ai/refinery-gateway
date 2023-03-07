973422d661fddc37ea0646618348baa886f19f4d
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-07 11:24:59 +0100

    Adds healthcheck for database

--- api/healthcheck.py
+++ api/healthcheck.py
@@ -5,13 +5,17 @@ from submodules.model.business_objects import general
 
 class Healthcheck(HTTPEndpoint):
     def get(self, request) -> PlainTextResponse:
-        error_message = ""
+        return PlainTextResponse("OK")
+
+
+class DatabaseHealthcheck(HTTPEndpoint):
+    def get(self, request) -> PlainTextResponse:
+        response = "OK"
 
         if not __test_database_connection():
-            error_message += " database_conncetion "
+            response = "FAILED"
 
-        text_response = error_message if error_message else "OK"
-        return PlainTextResponse(text_response)
+        return PlainTextResponse(response)
 
 
 def __test_database_connection() -> bool:
