0715455a40c922c277ed73f9b1e7d64f6357a27e
Simon Degraf <125890757+SimonDegrafKern@users.noreply.github.com>
2023-03-08 16:02:48 +0100

    healthcheck extension (#121)
    * Updates submodule
    
    * Extends healthcheck with database logic
    
    * Adds healthcheck for database
    
    * Adds databasehealthcheck
    
    * Updates submodule
    
    * Cleans up database healthcheck endpoint
    
    * Updates submodule
    
    * Combines checks into one endpoint

--- api/healthcheck.py
+++ api/healthcheck.py
@@ -1,7 +1,12 @@
 from starlette.endpoints import HTTPEndpoint
 from starlette.responses import PlainTextResponse
+from submodules.model.business_objects import general
 
 
 class Healthcheck(HTTPEndpoint):
     def get(self, request) -> PlainTextResponse:
-        return PlainTextResponse("OK")
+        headers = {"APP": "OK"}
+        database_test = general.test_database_connection()
+        if not database_test.get("success"):
+            headers["DATABASE"] = database_test.get("error")
+        return PlainTextResponse("OK", headers=headers)
