c09e129614d5a88997d25e961307fe042c284a28
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-08 13:34:23 +0100

    Combines checks into one endpoint

--- api/healthcheck.py
+++ api/healthcheck.py
@@ -5,10 +5,8 @@ from submodules.model.business_objects import general
 
 class Healthcheck(HTTPEndpoint):
     def get(self, request) -> PlainTextResponse:
-        return PlainTextResponse("OK")
-
-
-class DatabaseHealthcheck(HTTPEndpoint):
-    def get(self, request) -> PlainTextResponse:
-        response = "OK" if general.test_database_connection() else "FAILED"
-        return PlainTextResponse(response)
+        headers = {"APP": "OK"}
+        database_test = general.test_database_connection()
+        if not database_test.get("success"):
+            headers["DATABASE"] = database_test.get("error")
+        return PlainTextResponse("OK", headers=headers)
