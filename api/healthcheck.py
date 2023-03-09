18b2d1ebfb29e18eb74ba322e350fb4f934c678b
SimonDegrafKern <simon.degraf@kern.ai>
2023-03-09 10:34:54 +0100

    Adds status code and removes header

--- api/healthcheck.py
+++ api/healthcheck.py
@@ -5,8 +5,13 @@ from submodules.model.business_objects import general
 
 class Healthcheck(HTTPEndpoint):
     def get(self, request) -> PlainTextResponse:
-        headers = {"APP": "OK"}
+        text = ""
+        status_code = 200
         database_test = general.test_database_connection()
         if not database_test.get("success"):
-            headers["DATABASE"] = database_test.get("error")
-        return PlainTextResponse("OK", headers=headers)
+            error_name = database_test.get("error")
+            text += f"database_error:{error_name}:"
+            status_code = 500
+        if not text:
+            text = "OK"
+        return PlainTextResponse(text, status_code=status_code)
