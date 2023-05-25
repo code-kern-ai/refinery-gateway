4f916aaa108a175861c36b5699bf13fa1d7e5167
SimonDegrafKern <simon.degraf@kern.ai>
2023-05-25 14:42:22 +0200

    Fixes logic

--- controller/attribute/util.py
+++ controller/attribute/util.py
@@ -91,23 +91,20 @@ def run_attribute_calculation_exec_env(
     container = client.containers.create(
         image=image,
         command=command,
-        remove=True,
+        auto_remove=True,
         detach=True,
         network=exec_env_network,
     )
     __containers_running[container_name] = True
     daemon.run(__read_container_logs_thread, project_id, container_name, attribute_id, container)
     container.start()
-
-    # final log preparation
-    logs = [
+    attribute_item.logs = [
         line.decode("utf-8").strip("\n")
         for line in container.logs(
             stream=True, stdout=True, stderr=True, timestamps=True
         )
         if "progress" not in line.decode("utf-8")
     ]
-
     del __containers_running[container_name]
     __update_progress(project_id, attribute_item, 1.0) 
 
@@ -135,10 +132,8 @@ def __read_container_logs_thread(
 ):
 
     ctx_token = general.get_ctx_token()
-    # needs to be refetched since it is not thread safe
+
     attribute_item = attribute.get(project_id, attribute_id)
-    previous_progress = -1
-    last_timestamp = None
     c = 0
     while container_name in __containers_running:
         time.sleep(1)
@@ -149,57 +144,24 @@ def __read_container_logs_thread(
         if not container_name in __containers_running:
             break
         try:
-            log_lines = docker_container.logs(
+            last_progress = 0.0
+            for log in docker_container.logs(
+                stream=True,
                 stdout=True,
                 stderr=True,
                 timestamps=True,
-                since=last_timestamp,
-            )
-        except:
-            # failsafe for containers that shut down during the read
-            break
-        current_logs = [
-            l for l in str(log_lines.decode("utf-8")).split("\n") if len(l.strip()) > 0
-        ]
-
-        if len(current_logs) == 0:
+            ):
+                log = log.decode("utf-8")
+                if ":progress:" in log:
+                    progress = float(log.split(":")[-1])
+                    if progress > last_progress:
+                        last_progress = progress
+                        __update_progress(project_id, attribute_item, progress)
+        except Exception as e:
+            print("Exception", str(e), flush=True)
             continue
-        last_entry = current_logs[-1]
-        last_timestamp_str = last_entry.split(" ")[0]
-        last_timestamp = parser.parse(last_timestamp_str).replace(
-            tzinfo=None
-        ) + timedelta(seconds=1)
-        progress_logs = [l for l in current_logs if "progress" in l]
-        if len(progress_logs) == 0:
-            continue
-        progress = float(progress_logs[-1].split(":")[-1].strip())
-        if previous_progress == last_entry:
-            continue
-        previous_progress = last_entry
-        __update_progress(
-            project_id, attribute_item, progress
-        )
     general.remove_and_refresh_session(ctx_token)
 
-
-def __update_logs(
-    project_id: str,
-    attribute_item: Attribute,
-    logs: List[str],
-    ) -> None:
-    if not logs or len(logs) == 0:
-        return
-
-    if not attribute_item.logs:
-        attribute_item.logs = logs
-    else:
-        all_logs = [l for l in attribute_item.logs]
-        all_logs += logs
-        attribute_item.logs = all_logs
-    general.commit()
-    # currently dummy since frontend doesn't have a log change yet
-    message = f"calculate_attribute:logs:{attribute_item.id}"
-    notification.send_organization_update(project_id, message)
 
 def __update_progress(project_id: str, attribute_item: Attribute, progress: float) -> None:
     attribute_item.progress = progress
