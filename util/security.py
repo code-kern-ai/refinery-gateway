67db9eca851cf8b517165e20426c837e8b8ebb6a
SimonDegrafKern <simon.degraf@kern.ai>
2023-06-20 17:12:11 +0200

    Makes secret key owrking

--- util/security.py
+++ util/security.py
@@ -1,16 +1,15 @@
 
 import os
-import base64
 from typing import ByteString
 from cryptography.fernet import Fernet
 
 
-SECRET_KEY = os.getenv("FERNET_SECRET_KEY")
+FERNET_SECRET_KEY = "FERNET_SECRET_KEY"
 __fernet = None
 
 def set_secret_key() -> None:
     secret_key = Fernet.generate_key()
-    os.environ["FERNET_SECRET_KEY"] = base64.decodebytes(secret_key)
+    os.environ[FERNET_SECRET_KEY] = secret_key.decode()
 
 def ecrypt(value: str) -> ByteString:
     global __fernet
@@ -21,10 +20,11 @@ def decrypt(token: ByteString) -> ByteString:
     return __get_fernet().decrypt(token)
 
 def __get_fernet() -> Fernet:
-    if not os.getenv("FERNET_SECRET_KEY"):
+    if not os.getenv(FERNET_SECRET_KEY):
         set_secret_key()
 
     global __fernet
     if not __fernet:
-        __fernet = Fernet(base64.encode(SECRET_KEY))
+        __fernet = Fernet(os.getenv(FERNET_SECRET_KEY))
     return __fernet
+
