8c37941851f3144369fadafb090f33dcc85ad31e
SimonDegrafKern <simon.degraf@kern.ai>
2023-06-28 11:45:40 +0200

    Removes unnessecary global usage and formats code

--- util/security.py
+++ util/security.py
@@ -1,4 +1,3 @@
-
 import os
 import rncryptor
 
@@ -9,25 +8,26 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 __crypto_suite = None
 
 
-def encrypt(value: str) -> ByteString:
-    if not value:
-        return None
-    global __crypto_suite
-    value = __get_crypto_suite().encrypt(value, SECRET_KEY)
-    return value
-
-def decrypt(value: ByteString) -> str:
-    if not value:
-        return None
-    global __crypto_suite
-    value = __get_crypto_suite().decrypt(value, SECRET_KEY)
-    return value
-
 def __get_crypto_suite() -> rncryptor.RNCryptor:
     global __crypto_suite
     if not __crypto_suite:
         __crypto_suite = rncryptor.RNCryptor()
     return __crypto_suite
+
+
+def encrypt(value: str) -> ByteString:
+    if not value:
+        return None
+    value = __get_crypto_suite().encrypt(value, SECRET_KEY)
+    return value
+
+
+def decrypt(value: ByteString) -> str:
+    if not value:
+        return None
+    value = __get_crypto_suite().decrypt(value, SECRET_KEY)
+    return value
+
 
 def check_secret_key() -> None:
     if not SECRET_KEY:
