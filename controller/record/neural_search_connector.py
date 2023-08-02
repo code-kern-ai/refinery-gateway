14a976534c31d71e9e75fa7bb43c3c81e56ffd39
Lina <lina.lumburovska@kern.ai>
2023-08-02 16:05:01 +0200

    PR comments

--- controller/record/neural_search_connector.py
+++ controller/record/neural_search_connector.py
@@ -15,6 +15,5 @@ def request_most_similar_record_ids(
 ) -> List[str]:
     url = f"{BASE_URI}/most_similar?project_id={project_id}&embedding_id={embedding_id}&record_id={record_id}&limit={limit}"
 
-    # changed from get to post so we can send the filter -> however currently filter isn't part of the prototype so None
     result = service_requests.post_call_or_raise(url, att_filter)
     return result
