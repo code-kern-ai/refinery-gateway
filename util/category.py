484d7f025ef7f0ae89fa047f536194deac39a9e0
JWittmeyer <jens.wittmeyer@kern.ai>
2023-12-12 14:59:18 +0100

    Adds max size to integer columns & new category strategy

--- util/category.py
+++ util/category.py
@@ -14,18 +14,22 @@ def infer_category(file_name: str) -> str:
 def infer_category_enum(df: pd.DataFrame, df_col: str) -> str:
     type_name = df[df_col].dtype.name
     if type_name == "int64":
+        if df[df_col].apply(lambda x: x > 2_147_483_647).sum() > 0:
+            # doesn't fit in database INTEGER type
+            # check all values instead of sample since it a simple integer column
+            return enums.DataTypes.TEXT.value
         return enums.DataTypes.INTEGER.value
     elif type_name == "float64":
         return enums.DataTypes.FLOAT.value
     elif type_name == "bool":
         return enums.DataTypes.BOOLEAN.value
     elif type_name == "object":
-        sample = df[df_col].sample(10) if len(df) > 10 else df[df_col]
-        if sample.apply(lambda x: len(str(x).split()) > 4).sum() > 0:
-            # if any of 10 randomly sampled texts contains more than 4 whitespaces, it is most likely text
-            return enums.DataTypes.TEXT.value
-        else:
+        if (
+            df[df_col].nunique() <= df[df_col].count() * 0.2
+            and df[df_col].str.len().max() < 50
+        ):
             return enums.DataTypes.CATEGORY.value
+        return enums.DataTypes.TEXT.value
     else:
         return enums.DataTypes.UNKNOWN.value
 
