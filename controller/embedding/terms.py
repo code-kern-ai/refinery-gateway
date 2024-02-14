58ddb3f1a5ab71f45f48dbb30e3cd6628b9b934e
JWittmeyer <jens.wittmeyer@kern.ai>
2024-02-14 11:11:19 +0100

    Reincludes cohere as provider

--- controller/embedding/terms.py
+++ controller/embedding/terms.py
@@ -11,6 +11,11 @@ TERMS_INFO = {
         "terms": "Please note that by enabling this third-party API, you are stating that you accept its addition as a sub-processor under the terms of our Data Processing Agreement. Please be aware that the OpenAI API policies may conflict with your internal data and privacy policies. For more information please check: @@PLACEHOLDER@@. For questions you can contact us at security@kern.ai.",
         "link": "https://openai.com/policies/api-data-usage-policies",
     },
+    EmbeddingPlatform.COHERE.value:{
+        "platform": EmbeddingPlatform.COHERE.value,
+        "terms": "Please note that by enabling this third-party API, you are stating that you accept its addition as a sub-processor under the terms of our Data Processing Agreement. Please be aware that the Cohere API policies may conflict with your internal data and privacy policies. For more information please check: @@PLACEHOLDER@@. For questions you can contact us at security@kern.ai.",
+        "link": "https://cohere.com/terms-of-use",
+    },
     EmbeddingPlatform.PYTHON.value: {
         "platform": EmbeddingPlatform.PYTHON.value,
         "terms": None,
