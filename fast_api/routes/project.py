04f44d60eb76ced96bf0429d19a0da409fe5cbd3
anmarhindi <anmar.hindi@kern.ai>
2024-03-15 14:13:50 +0100

    changes to function name

--- fast_api/routes/project.py
+++ fast_api/routes/project.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Request
 from fast_api.routes.client_response import pack_json_result
 from typing import Dict
 from controller.auth import manager as auth
-from submodules.model.business_objects.project import _get_project_by_project_id
+from submodules.model.business_objects.project import get_project_by_project_id_sql
 
 
 router = APIRouter()
@@ -14,6 +14,6 @@ async def get_project_by_project_id(request: Request, project_id: str) -> Dict:
     # auth.check_demo_access(info)
     # auth.check_project_access(info, project_id)
 
-    data = _get_project_by_project_id(project_id)
+    data = get_project_by_project_id_sql(project_id)
 
     return pack_json_result({"data": {"projectByProjectId": data}})
