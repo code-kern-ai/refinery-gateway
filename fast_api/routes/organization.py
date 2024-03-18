e810973122bd1119fa3a45772231b35e12ca7517
anmarhindi <anmar.hindi@kern.ai>
2024-03-18 16:22:36 +0100

    added overview stats

--- fast_api/routes/organization.py
+++ fast_api/routes/organization.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request
 from controller.auth import manager as auth_manager
+from controller.organization import manager
 from fast_api.routes.fastapi_resolve_info import FastAPIResolveInfo
 
 
@@ -8,7 +9,17 @@ router = APIRouter()
 
 @router.get("/overview-stats")
 async def get_overview_stats(request: Request):
-    raise NotImplementedError
+    info = FastAPIResolveInfo(
+        context={"request": request},
+        field_name="OrganizationQuery",
+        parent_type="Query",
+    )
+
+    auth_manager.check_demo_access(info)
+    org_id = str(auth_manager.get_user_by_info(info).organization_id)
+    data = manager.get_overview_stats(org_id)
+
+    return {"data": {"overviewStats": data}}
 
 
 @router.get("/user-info")
