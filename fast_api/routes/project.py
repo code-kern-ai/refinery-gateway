63a368042a9fcab15e299b8c8a5ea31460fc37f9
anmarhindi <anmar.hindi@kern.ai>
2024-03-18 16:34:18 +0100

    auth manager added to project by project id

--- fast_api/routes/project.py
+++ fast_api/routes/project.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Request
 from fast_api.routes.client_response import pack_json_result
 from typing import Dict
-from controller.auth import manager as auth
+from controller.auth import manager as auth_manager
+from fast_api.routes.fastapi_resolve_info import FastAPIResolveInfo
 from submodules.model.business_objects.project import get_project_by_project_id_sql
 from controller.project import manager
 from submodules.model.util import sql_alchemy_to_dict, pack_as_graphql
@@ -11,10 +12,14 @@ router = APIRouter()
 
 @router.get("/project-by-project-id/{project_id}")
 async def get_project_by_project_id(request: Request, project_id: str) -> Dict:
+    info = FastAPIResolveInfo(
+        context={"request": request},
+        field_name="ProjectQuery",
+        parent_type="Query",
+    )
 
-    # TODO: Implement auth checks
-    # auth.check_demo_access(info)
-    # auth.check_project_access(info, project_id)
+    auth_manager.check_demo_access(info)
+    auth_manager.check_project_access(info, project_id)
 
     data = get_project_by_project_id_sql(project_id)
 
