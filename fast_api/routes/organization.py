22c2735912b43c951f6513609a8adc295df6b2ef
JWittmeyer <jens.wittmeyer@kern.ai>
2024-03-20 17:15:57 +0100

    Adds all active admin messages

--- fast_api/routes/organization.py
+++ fast_api/routes/organization.py
@@ -1,9 +1,21 @@
 from fastapi import APIRouter, Request
 from controller.auth import manager as auth_manager
 from controller.organization import manager
+from controller.admin_message import manager as admin_message_manager
 
+from fast_api.routes.client_response import pack_json_result
+from submodules.model.util import sql_alchemy_to_dict
 
 router = APIRouter()
+
+
+ACTIVE_ADMIN_MESSAGE_KEYS_TO_BE_KEPT = {
+    "archive_date",
+    "created_at",
+    "id",
+    "level",
+    "text",
+}
 
 
 @router.get("/overview-stats")
@@ -19,3 +31,24 @@ def get_user_info(request: Request):
     data = auth_manager.get_user_by_info(request.state.info)
 
     return {"data": {"userInfo": data}}
+
+
+@router.get("/all-active-admin-messages")
+def all_active_admin_messages(request: Request, limit: int = 100) -> str:
+    auth_manager.check_demo_access(request.state.info)
+    return pack_json_result(
+        {
+            "data": {
+                "allActiveAdminMessages": [
+                    {
+                        k: v
+                        for k, v in sql_alchemy_to_dict(ds).items()
+                        if k in ACTIVE_ADMIN_MESSAGE_KEYS_TO_BE_KEPT
+                    }
+                    for ds in admin_message_manager.get_messages(
+                        limit, active_only=True
+                    )
+                ]
+            }
+        },
+    )
