e84697e3dbcc931eb6abf053899e98ea610e47db
Lina <lina.lumburovska@kern.ai>
2024-03-21 11:45:46 +0100

    Lookup lists by project id

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ from fast_api.routes.attribute import router as attribute_router
 from fast_api.routes.embedding import router as embedding_router
 from fast_api.routes.notification import router as notification_router
 from fast_api.routes.data_slices import router as data_slice_router
+from fast_api.routes.lookup_lists import router as lookup_lists_router
 from middleware.database_session import DatabaseSessionHandler
 from starlette.applications import Starlette
 from starlette.graphql import GraphQLApp
@@ -43,6 +44,7 @@ from route_prefix import (
     PREFIX_EMBEDDING,
     PREFIX_NOTIFICATION,
     PREFIX_DATA_SLICE,
+    PREFIX_LOOKUP_LISTS,
 )
 from util import security, clean_up
 
@@ -69,6 +71,9 @@ fastapi_app.include_router(
 
 fastapi_app.include_router(
     data_slice_router, prefix=PREFIX_DATA_SLICE, tags=["data-slice"]
+)
+fastapi_app.include_router(
+    lookup_lists_router, prefix=PREFIX_LOOKUP_LISTS, tags=["lookup-lists"]
 )
 
 routes = [
