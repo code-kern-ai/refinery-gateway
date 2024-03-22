d812c6924c8bd202bc93c4b91782ecab402ee83e
Lina <lina.lumburovska@kern.ai>
2024-03-22 09:46:46 +0100

    Query attributeByAttributeId

--- fast_api/routes/project_setting.py
+++ fast_api/routes/project_setting.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Request
 from typing import Dict
 from controller.task_queue import manager
+from controller.attribute import manager as attribute_manager
 from fast_api.routes.client_response import pack_json_result
+from submodules.model.util import sql_alchemy_to_dict
 
 
 router = APIRouter()
@@ -13,3 +15,11 @@ def get_project_by_project_id(
 ) -> Dict:
     data = manager.get_all_waiting_by_type(project_id, task_type)
     return pack_json_result({"data": {"queuedTasks": data}})
+
+
+@router.get("/{project_id}/{attribute_id}/attribute-by-id")
+def get_attribute_by_attribute_id(project_id: str, attribute_id: str):
+    data = sql_alchemy_to_dict(
+        attribute_manager.get_attribute(project_id, attribute_id)
+    )
+    return pack_json_result({"data": {"attributeByAttributeId": data}})
