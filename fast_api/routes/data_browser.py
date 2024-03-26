daa6c2f4bc32d1bd0ae8d59df242d4b11fb95ccf
anmarhindi <anmar.hindi@kern.ai>
2024-03-26 17:48:10 +0100

    adds recordsByStaticSlice

--- fast_api/routes/data_browser.py
+++ fast_api/routes/data_browser.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from controller.auth import manager as auth_manager
@@ -99,3 +100,42 @@ def create_outlier_slice(request: Request, project_id: str, embedding_id: str):
         status_code=201,
         content={"message": "Outlier slice created"},
     )
+
+
+@router.post("/{project_id}/records-by-static-slice/{slice_id}")
+async def get_records_by_static_slice(request: Request, project_id: str, slice_id: str):
+    body = await request.body()
+
+    try:
+        data = json.loads(body)
+        order_by: Dict[str, str] = data.get("orderBy", {})
+        offset = data.get("offset", 0)
+        limit = data.get("limit", 0)
+    except json.JSONDecodeError:
+        return JSONResponse(
+            status_code=400,
+            content={"message": "Invalid JSON"},
+        )
+
+    user_id = auth_manager.get_user_id_by_info(request.state.info)
+
+    results = manager.get_records_by_static_slice(
+        user_id, project_id, slice_id, order_by, limit, offset
+    )
+
+    record_list = sql_alchemy_to_dict(results.record_list, for_frontend=False)
+    record_list = to_frontend_obj_raw(record_list)
+    record_list_pop = [
+        {"recordData": json.dumps(item), "__typename": "ExtendedRecord"}
+        for item in record_list
+    ]
+
+    data = {
+        "recordList": record_list_pop,
+        "queryLimit": results.query_limit,
+        "queryOffset": results.query_offset,
+        "fullCount": results.full_count,
+        "sessionId": results.session_id,
+    }
+
+    return pack_json_result({"data": {"recordsByStaticSlice": data}})
