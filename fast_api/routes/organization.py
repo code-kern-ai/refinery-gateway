c63202df4ac243b213cf19da6c19755265c1d8b8
anmarhindi <anmar.hindi@kern.ai>
2024-05-15 11:13:41 +0200

    adds changes to endpoints

--- fast_api/routes/organization.py
+++ fast_api/routes/organization.py
@@ -11,6 +11,7 @@ from fast_api.models import (
     DeleteOrganizationBody,
     RemoveUserToOrganizationBody,
     UpdateConfigBody,
+    UserLanguageDisplay,
 )
 from controller.auth import manager as auth_manager
 from controller.auth.kratos import (
@@ -74,7 +75,7 @@ def get_user_info_extended(request: Request):
     data = {
         "userInfo": {
             "id": user_id,
-            "organization": {"id": str(user.organization_id)},
+            "organizationId": str(user.organization_id),
             "firstName": name.get("first"),
             "lastName": name.get("last"),
             "mail": mail,
@@ -340,3 +341,9 @@ def archive_admin_message(
     )
     notification.send_global_update_for_all_organizations("admin_message")
     return pack_json_result({"data": {"archiveAdminMessage": {"ok": True}}})
+
+
+@router.post("/set-language-display")
+def set_language_display(request: Request, body: UserLanguageDisplay = Body(...)):
+    user_manager.update_user_language_display(body.user_id, body.language_display)
+    return pack_json_result({"data": {"changeUserLanguageDisplay": {"ok": True}}})
