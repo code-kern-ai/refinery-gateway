f6e6f12ded9ec5e2b4cc8eb0fefe22894c4d2f7b
lumburovskalina <104008550+lumburovskalina@users.noreply.github.com>
2024-08-29 10:38:11 +0200

    Task master dashboard (#244)
    * get all task simple
    
    * resolve name and org name
    
    * delete task from task queue db
    
    * model
    
    * Removed unused code
    
    * remove only running
    
    * submodules updated
    
    * Submodules merged
    
    ---------
    
    Co-authored-by: LennartSchmidtKern <lennart.schmidt@kern.ai>

--- fast_api/routes/misc.py
+++ fast_api/routes/misc.py
@@ -87,36 +87,23 @@ def model_provider_download_model(
 
 
 @router.get("/all-tasks")
-def get_all_tasks(request: Request, only_running: bool):
+def get_all_tasks(request: Request, page: int = 1, limit: int = 100):
     auth.check_admin_access(request.state.info)
-    tasks = controller_manager.monitor_all_tasks(only_running=only_running)
+    tasks = controller_manager.monitor_all_tasks(page=page, limit=limit)
+    return pack_json_result(tasks)
 
-    all_tasks = []
 
-    for task in tasks:
-        started_at = None
-        if task.started_at:
-            started_at = task.started_at.isoformat()
-
-        finished_at = None
-        if task.finished_at:
-            finished_at = task.finished_at.isoformat()
-
-        all_tasks.append(
-            {
-                "projectId": str(task.project_id),
-                "state": task.state,
-                "taskType": task.task_type,
-                "id": str(task.id),
-                "createdBy": task.created_by,
-                "organizationName": task.organization_name,
-                "projectName": task.project_name,
-                "startedAt": started_at,
-                "finishedAt": finished_at,
-            }
-        )
-
-    return pack_json_result({"data": {"allTasks": all_tasks}})
+@router.delete(
+    "/delete-from-task-queue-db",
+)
+def delete_from_task_queue_db(
+    request: Request,
+    task_id: str,
+    org_id: str,
+):
+    auth.check_admin_access(request.state.info)
+    task_master_manager.delete_task(org_id, task_id)
+    return SILENT_SUCCESS_RESPONSE
 
 
 @router.post("/cancel-task")
