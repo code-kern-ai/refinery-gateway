b746cc1589fde642b1196a7bb588afd4104fada1
JWittmeyer <jens.wittmeyer@kern.ai>
2024-11-06 11:58:43 +0100

    Removes overview charts but label distribution

--- fast_api/routes/project.py
+++ fast_api/routes/project.py
@@ -11,15 +11,13 @@ from fast_api.models import (
 )
 from fastapi import APIRouter, Body, Depends, Request
 from fast_api.routes.client_response import pack_json_result
-from typing import Dict, List
+from typing import Dict
 from controller.auth import manager as auth_manager
 from controller.attribute import manager as attr_manager
-from controller.labeling_task import manager as task_manager
 from controller.upload_task import manager as upload_task_manager
 from submodules.model.business_objects import information_source, labeling_task
-from submodules.model import enums, events
+from submodules.model import enums
 from submodules.model.business_objects.embedding import get_all_embeddings_by_project_id
-from submodules.model.enums import LabelingTaskType
 from submodules.model.business_objects.project import get_project_by_project_id_sql
 from submodules.model.business_objects.labeling_task import (
     get_labeling_tasks_by_project_id_full,
@@ -33,10 +31,6 @@ from submodules.model.util import (
     to_frontend_obj_raw,
 )
 from util import notification
-from util.inter_annotator.functions import (
-    resolve_inter_annotator_matrix_classification,
-    resolve_inter_annotator_matrix_extraction,
-)
 from controller.misc import manager as misc
 from exceptions.exceptions import NotAllowedInOpenSourceError
 from submodules.model.business_objects import notification as notification_model
@@ -130,87 +124,6 @@ def general_project_stats(
             "data": {
                 "generalProjectStats": manager.get_general_project_stats(
                     project_id, labeling_task_id, slice_id
-                )
-            }
-        },
-        wrap_for_frontend=False,  # not wrapped as the prepared results in snake_case are still the expected form the frontend
-    )
-
-
-@router.get(
-    "/{project_id}/inter-annotator-matrix",
-    dependencies=[Depends(auth_manager.check_project_access_dep)],
-)
-def inter_annotator_matrix(
-    project_id: str,
-    labeling_task_id: str,
-    include_gold_star: Optional[bool] = True,
-    include_all_org_user: Optional[bool] = False,
-    only_on_static_slice: Optional[str] = None,
-) -> Dict:
-
-    labeling_task = task_manager.get_labeling_task(project_id, labeling_task_id)
-    if not labeling_task:
-        raise ValueError("Can't match labeling task to given Ids")
-    fp = None
-    if labeling_task.task_type == LabelingTaskType.CLASSIFICATION.value:
-        fp = resolve_inter_annotator_matrix_classification
-    elif labeling_task.task_type == LabelingTaskType.INFORMATION_EXTRACTION.value:
-        fp = resolve_inter_annotator_matrix_extraction
-    else:
-        raise ValueError(f"Can't match task type {labeling_task.task_type}")
-
-    return pack_json_result(
-        {
-            "data": {
-                "interAnnotatorMatrix": fp(
-                    labeling_task,
-                    include_gold_star,
-                    include_all_org_user,
-                    only_on_static_slice,
-                )
-            }
-        },
-        wrap_for_frontend=False,  # not wrapped as the prepared results in snake_case are still the expected form the frontend
-    )
-
-
-@router.get(
-    "/{project_id}/confusion-matrix",
-    dependencies=[Depends(auth_manager.check_project_access_dep)],
-)
-def confusion_matrix(
-    project_id: str,
-    labeling_task_id: str,
-    slice_id: Optional[str] = None,
-) -> Dict:
-    return pack_json_result(
-        {
-            "data": {
-                "confusionMatrix": manager.get_confusion_matrix(
-                    project_id, labeling_task_id, slice_id
-                )
-            }
-        },
-        wrap_for_frontend=False,  # not wrapped as the prepared results in snake_case are still the expected form the frontend
-    )
-
-
-@router.get(
-    "/{project_id}/confidence-distribution",
-    dependencies=[Depends(auth_manager.check_project_access_dep)],
-)
-def confidence_distribution(
-    project_id: str,
-    labeling_task_id: Optional[str] = None,
-    slice_id: Optional[str] = None,
-    num_samples: int = 100,
-) -> List:
-    return pack_json_result(
-        {
-            "data": {
-                "confidenceDistribution": manager.get_confidence_distribution(
-                    project_id, labeling_task_id, slice_id, num_samples
                 )
             }
         },
