3a3efc4a7827589425255ba39d1026d01a99f81c
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-01-14 17:35:01 +0100

    similar records

--- fast_api/routes/playground.py
+++ fast_api/routes/playground.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends, Request, Body
 from controller.auth import manager as auth_manager
-from fast_api.routes.client_response import pack_json_result
+from fast_api.routes.client_response import pack_json_result, GENERIC_FAILURE_RESPONSE
 from fast_api.models import SearchQuestionBody
-from typing import List, Any, Optional, Tuple
+from typing import List, Any, Optional, Tuple, Dict
 import os
 import requests
 
@@ -10,6 +10,9 @@ NEURAL_SEARCH = os.getenv("NEURAL_SEARCH")
 EMBEDDING_SERVICE = os.getenv("EMBEDDING_SERVICE")
 
 router = APIRouter()
+LIMIT = 10
+FILTER = None
+THRESHOLD = None
 
 
 @router.post(
@@ -23,10 +26,17 @@ def get_search_results(
     embedding_id = search_question.embeddingId
     question = search_question.question
 
-    has_error, result, logs = get_tensors_for_texts(
-        project_id, embedding_id, [question]
+    question_tensor = get_tensors_for_texts(project_id, embedding_id, [question])
+
+    if question_tensor is None:
+        return GENERIC_FAILURE_RESPONSE
+    elif len(question_tensor) == 0:
+        return pack_json_result([])
+    search_results = get_most_similar_records(
+        project_id, embedding_id, question_tensor[0], LIMIT
     )
-    return pack_json_result({"has_error": has_error, "result": result, "logs": logs})
+
+    return pack_json_result(search_results)
 
 
 def get_tensors_for_texts(
@@ -45,5 +55,36 @@ def get_tensors_for_texts(
     )
     if response.ok:
         return response.json()["tensor"]
+    else:
+        return None
+
+
+def get_most_similar_records(
+    project_id: str,
+    embedding_id: str,
+    embedding_tensor: List[float],
+    limit: int,
+    similarity_filter_option: Optional[List[Dict[str, Any]]] = None,
+    threshold: Optional[float] = None,
+):
+    url = f"{NEURAL_SEARCH}/most_similar_by_embedding?include_scores=true"
+
+    response = requests.post(
+        url,
+        headers={
+            "accept": "application/json",
+            "content-type": "application/json",
+        },
+        json={
+            "project_id": project_id,
+            "embedding_id": embedding_id,
+            "embedding_tensor": embedding_tensor,
+            "limit": limit,
+            "att_filter": similarity_filter_option,
+            "threshold": threshold,
+        },
+    )
+    if response.ok:
+        return response.json()
     else:
         return None
