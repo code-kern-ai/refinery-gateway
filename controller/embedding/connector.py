304da3e0c5f65058d9ce6cfc5d0f0bf9fad907f1
anmarhindi <anmar.hindi@kern.ai>
2025-02-06 10:15:10 +0100

    Rename base uri

--- controller/embedding/connector.py
+++ controller/embedding/connector.py
@@ -5,17 +5,17 @@ from exceptions.exceptions import EmbeddingConnectorError
 from util import service_requests
 import requests
 
-BASE_URI = os.getenv("EMBEDDING_SERVICE")
+EMBEDDING_BASE_URI = os.getenv("EMBEDDING_SERVICE")
 NEURAL_SEARCH_BASE_URI = os.getenv("NEURAL_SEARCH")
 
 
 def request_listing_recommended_encoders() -> Any:
-    url = f"{BASE_URI}/classification/recommend/TEXT"  # TODO does here have to be a data type?
+    url = f"{EMBEDDING_BASE_URI}/classification/recommend/TEXT"  # TODO does here have to be a data type?
     return service_requests.get_call_or_raise(url)
 
 
 def request_embedding(project_id: str, embedding_id: str) -> Any:
-    url = f"{BASE_URI}/embed"
+    url = f"{EMBEDDING_BASE_URI}/embed"
     data = {
         "project_id": str(project_id),
         "embedding_id": str(embedding_id),
@@ -24,12 +24,12 @@ def request_embedding(project_id: str, embedding_id: str) -> Any:
 
 
 def request_deleting_embedding(project_id: str, embedding_id: str) -> Any:
-    url = f"{BASE_URI}/delete/{project_id}/{embedding_id}"
+    url = f"{EMBEDDING_BASE_URI}/delete/{project_id}/{embedding_id}"
     return service_requests.delete_call_or_raise(url)
 
 
 def request_tensor_upload(project_id: str, embedding_id: str) -> None:
-    url = f"{BASE_URI}/upload_tensor_data/{project_id}/{embedding_id}"
+    url = f"{EMBEDDING_BASE_URI}/upload_tensor_data/{project_id}/{embedding_id}"
     service_requests.post_call_or_raise(url, {})
 
 
@@ -39,7 +39,7 @@ def request_re_embed_records(
     # example changes structure:
     # {"<embedding_id>":[{"record_id":"<record_id>","attribute_name":"<attribute_name>","sub_key":"<sub_key>"}]}
     # note that sub_key is optional and only for embedding lists relevant
-    url = f"{BASE_URI}/re_embed_records/{project_id}"
+    url = f"{EMBEDDING_BASE_URI}/re_embed_records/{project_id}"
     service_requests.post_call_or_raise(url, {"changes": changes})
 
 
