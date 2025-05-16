3d36c652957725af6b194902c40213fbee04b2b5
andhreljaKern <andrea.hrelja@kern.ai>
2025-05-16 11:50:56 +0200

    perf: update integration last_extraction

--- alembic/versions/37daf20132b9_adds_integration_providers.py
+++ alembic/versions/37daf20132b9_adds_integration_providers.py
@@ -1,8 +1,8 @@
 """adds integration providers
 
-Revision ID: 3c00d99b37aa
+Revision ID: 37daf20132b9
 Revises: eb96f9b82cc1
-Create Date: 2025-05-16 06:56:43.813256
+Create Date: 2025-05-16 09:50:22.035523
 
 """
 
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = "3c00d99b37aa"
+revision = "37daf20132b9"
 down_revision = "eb96f9b82cc1"
 branch_labels = None
 depends_on = None
@@ -60,6 +60,7 @@ def upgrade():
         sa.Column("config", sa.JSON(), nullable=True),
         sa.Column("llm_config", sa.JSON(), nullable=True),
         sa.Column("error_message", sa.String(), nullable=True),
+        sa.Column("last_extraction", sa.JSON(), nullable=True),
         sa.ForeignKeyConstraint(["created_by"], ["user.id"], ondelete="SET NULL"),
         sa.ForeignKeyConstraint(["project_id"], ["project.id"], ondelete="CASCADE"),
         sa.PrimaryKeyConstraint("id"),
