1b0bd868e33e53f6c0192a6ec914e80e6a041778
andhreljaKern <andrea.hrelja@kern.ai>
2025-05-16 22:13:19 +0200

    perf: task manipulation

--- controller/monitor/manager.py
+++ controller/monitor/manager.py
@@ -115,3 +115,15 @@ def cancel_parse_cognition_file_task(
         transformation_key,
         with_commit=True,
     )
+
+
+def cancel_integration_task(
+    task_info: Dict[str, Any],
+) -> None:
+
+    integration_id = task_info.get("integrationId")
+
+    task_monitor.set_integration_task_to_failed(
+        integration_id,
+        with_commit=True,
+    )
--- fast_api/routes/misc.py
+++ fast_api/routes/misc.py
@@ -129,6 +129,8 @@ def cancel_task(
             controller_manager.cancel_parse_cognition_file_task(
                 task_entity.organization_id, task_info
             )
+        elif task_type == enums.TaskType.INTEGRATION.value:
+            controller_manager.cancel_integration_task(task_info)
         else:
             raise ValueError(f"{task_type} is no valid task type")
 
