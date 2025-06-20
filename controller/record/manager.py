a12d82caefcd0d7dd62b731c7173e43b81027816
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-06-20 14:23:57 +0200

    endpoint for sync internal

--- controller/record/manager.py
+++ controller/record/manager.py
@@ -13,6 +13,7 @@ from submodules.model.business_objects import (
     task_queue,
     record_label_association,
     comments,
+    project
 )
 from service.search import search
 from submodules.model import enums
@@ -30,6 +31,8 @@ from util import notification
 import time
 import traceback
 from controller.embedding import connector
+from submodules.model.cognition_objects import group as group_db
+import json
 
 
 def get_record(project_id: str, record_id: str) -> Record:
@@ -331,6 +334,53 @@ def delete_records(
         )
     else:
         __delete_records(project_id, record_ids)
+
+
+def sync_access_groups_and_users_sharepoint(project_id: str, integration_id: str, permissions_users: Dict[str, Any], record_ids: Optional[List[str]]) -> None:
+    try:
+        if record_ids:
+            project_records = record.get_by_record_ids(project_id, record_ids)
+        else:
+            project_records = record.get_all(project_id)
+        organization_id = project.get_org_id(project_id)
+        integration_groups_by_permission_id = group_db.get_all_by_integration_id_permission_grouped(organization_id, integration_id)
+        record_change_dict = {}
+        for record_item in project_records:
+            if not record_item.data.get("__ACCESS_GROUPS"):
+                current_group_ids = []
+            else:
+                current_group_ids = record_item.data["__ACCESS_GROUPS"]
+
+            meta_data_dict = json.loads(record_item.data.get("metadata", "{}"))
+            permission_ids = meta_data_dict.get("permissions")
+            new_group_ids = [str(integration_groups_by_permission_id.get(permission_id).id) for permission_id in permission_ids if integration_groups_by_permission_id.get(permission_id)]  
+            extended_group_ids = list(set(current_group_ids + new_group_ids))  # remove duplicates
+            record_change_dict[f"{str(record_item.id)}@__ACCESS_GROUPS"] = {
+                "attributeName": "__ACCESS_GROUPS",
+                "newValue": extended_group_ids,
+                "recordId": str(record_item.id),
+            }
+            if not record_item.data.get("__ACCESS_USERS"):
+                current_user_ids = []
+            else:
+                current_user_ids = record_item.data["__ACCESS_USERS"]
+                new_user_ids = [permissions_users.get(permission_id) for permission_id in permission_ids if permissions_users.get(permission_id)]
+                if new_user_ids:
+                    extended_user_ids = list(set(current_user_ids + new_user_ids))
+                    record_change_dict[f"{str(record_item.id)}@__ACCESS_USERS"] = {
+                        "attributeName": "__ACCESS_USERS",
+                        "newValue": extended_user_ids,
+                        "recordId": str(record_item.id),
+                    }
+
+        errors = edit_records(None, project_id, record_change_dict, True)
+        if not errors:
+            all_embeddings = embedding.get_all_embeddings_by_project_id(project_id)
+            for embedding_item in all_embeddings:
+                connector.update_attribute_payloads_for_neural_search(project_id, str(embedding_item.id))
+    except Exception:
+        print(traceback.format_exc(), flush=True)
+        raise Exception("Error syncing access groups and users from SharePoint integration")
 
 
 def add_access_groups_or_users(project_id: str, record_ids: List[str], group_ids: Optional[List[str]] = None, user_ids: Optional[List[str]] = None) -> None:
