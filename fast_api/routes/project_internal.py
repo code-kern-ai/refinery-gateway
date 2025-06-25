53930c419b0536acd39d1eccc5439c54f2d1b2f0
andhreljaKern <andrea.hrelja@kern.ai>
2025-06-25 16:26:55 +0200

    perf(alembic): db update perf: rename internal delete endpoint

--- fast_api/routes/project_internal.py
+++ fast_api/routes/project_internal.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Request
 
-from controller.auth import manager as auth_manager
 from controller.project import manager
 from fast_api.models import ProjectDeletion
 from fast_api.routes.client_response import get_silent_success
 from util import notification
 
 from submodules.model import enums
-from submodules.model.business_objects import notification as notification_model
+from submodules.model.business_objects import (
+    notification as notification_model,
+    user as user_db_bo,
+)
 
 router = APIRouter()
 
@@ -15,10 +17,10 @@ router = APIRouter()
 @router.delete(
     "/delete-projects",
 )
-def delete_project(request: Request, data: ProjectDeletion):
+def delete_projects(request: Request, data: ProjectDeletion):
     for project_id in data.project_ids:
         manager.update_project(project_id, status=enums.ProjectStatus.IN_DELETION.value)
-        user = auth_manager.get_user_by_info(data.user_id)
+        user = user_db_bo.get(data.user_id)
         project_item = manager.get_project(project_id)
         organization_id = str(project_item.organization_id)
         notification.create_notification(
