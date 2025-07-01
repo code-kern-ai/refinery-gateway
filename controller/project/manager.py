d9e32e9b1cdefd60bba9f3d91f48960dad500e5c
LennartSchmidtKern <lennart.schmidt@kern.ai>
2025-07-01 11:09:48 +0200

    active

--- controller/project/manager.py
+++ controller/project/manager.py
@@ -64,7 +64,6 @@ def get_all_projects(organization_id: str) -> List[Project]:
 def get_all_projects_with_access_management(organization_id: str) -> List[Project]:
     all_projects = project.get_all_with_access_management(organization_id)
     all_projects_dict = sql_alchemy_to_dict(all_projects)
-    print("all_projects", all_projects_dict, type(all_projects_dict))
     return all_projects_dict
 
 
