dfa0f9888982f0f71d94420090179fd80cb70098
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2025-08-07 15:12:58 +0200

    Fixes no api key embedding name endpoint (#337)
    * Fixes no api key embedding name endpoint
    
    * privatemode ai for ac
    
    * Fix unbound session in debounce
    
    * Adds terms for privatemode ai
    
    * PR comment
    
    * Submodule merge

--- controller/embedding/terms.py
+++ controller/embedding/terms.py
@@ -16,4 +16,9 @@ TERMS_INFO = {
         "terms": "Please note that by enabling this third-party API, you are stating that you accept its addition as a sub-processor under the terms of our Data Processing Agreement. Please be aware that the Azure API policies may conflict with your internal data and privacy policies. For more information please check: @@PLACEHOLDER@@. For questions you can contact us at security@kern.ai.",
         "link": "https://www.microsoft.com/en-us/legal/terms-of-use",
     },
+    EmbeddingPlatform.PRIVATEMODE_AI.value: {
+        "platform": EmbeddingPlatform.PRIVATEMODE_AI.value,
+        "terms": "Please note that by enabling this third-party API, you are stating that you accept its addition as a sub-processor under the terms of our Data Processing Agreement. Please be aware that the PrivateMode AI API policies may conflict with your internal data and privacy policies. For more information please check: @@PLACEHOLDER@@. For questions you can contact us at security@kern.ai.",
+        "link": "https://www.privatemode.ai/terms-of-service",
+    },
 }
