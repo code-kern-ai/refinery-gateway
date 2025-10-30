0b7c896e295259f1e7dca1528d075b36b4fbaf56
andhreljaKern <andrea.hrelja@kern.ai>
2025-10-30 10:16:02 +0100

    perf: update minio_upload for execute_etl

--- controller/transfer/cognition/minio_upload.py
+++ controller/transfer/cognition/minio_upload.py
@@ -1,14 +1,14 @@
 from typing import List
 
-from controller.transfer.cognition import etl as etl_util
 from controller.task_master import manager as task_master_manager
+from submodules.model import enums
+from submodules.model.business_objects import general
+from submodules.model.global_objects import etl_task as etl_task_bo
 from submodules.model.cognition_objects import (
     file_reference as file_reference_db_bo,
     markdown_file as markdown_file_bo,
     markdown_dataset as markdown_dataset_bo,
 )
-from submodules.model import enums
-from submodules.model.business_objects import general
 
 
 def handle_cognition_file_upload(path_parts: List[str]):
@@ -37,6 +37,7 @@ def handle_cognition_file_upload(path_parts: List[str]):
         file_reference.state = enums.FileCachingState.COMPLETED.value
         general.commit()
 
+        chunk_size = 1000
         priority = -1
         if (
             file_reference.meta_data.get("transformation_initiator")
@@ -76,19 +77,61 @@ def handle_cognition_file_upload(path_parts: List[str]):
         markdown_dataset = markdown_dataset_bo.get(
             org_id=org_id, id=markdown_file.dataset_id
         )
-        file_type = enums.ETLFileType.from_string(markdown_file.category_origin)
-        etl_task = etl_util.get_or_create_task(
+
+        etl_task = etl_task_bo.get_or_create_markdown_file_etl_task(
+            org_id=org_id,
+            file_reference=file_reference,
             markdown_file=markdown_file,
             markdown_dataset=markdown_dataset,
-            minio_path=file_reference.minio_path,
-            original_file_name=file_reference.original_file_name,
-            file_size_bytes=file_reference.file_size_bytes,
-            file_type=file_type,
-            extractor=enums.ETLExtractorPDF.from_string(
-                markdown_file.meta_data.get("extractor")
-            ),
-            split_strategy=markdown_file.meta_data.get("split_strategy"),
-            chunk_size=markdown_file.meta_data.get("chunk_size"),
+            extractor=markdown_file.meta_data.get("extractor"),
+            fallback_extractors=[
+                enums.ETLExtractorPDF.PDF2MD,
+                enums.ETLExtractorPDF.VISION,
+            ],
+            cache_config={
+                "use_file_cache": True,
+                "use_extraction_cache": False,
+                "use_transformation_cache": True,
+            },
+            split_config={
+                "strategy": enums.ETLSplitStrategy.CHUNK.value,
+                "chunk_size": chunk_size,
+            },
+            transform_config={
+                "transformers": [
+                    {  # NOTE: __call_gpt_with_key only reads user_prompt
+                        "enabled": True,
+                        "name": enums.ETLTransformer.CLEANSE.value,
+                        "system_prompt": None,
+                        "user_prompt": None,
+                    },
+                    {
+                        "enabled": True,
+                        "name": enums.ETLTransformer.TEXT_TO_TABLE.value,
+                        "system_prompt": None,
+                        "user_prompt": None,
+                    },
+                    {
+                        "enabled": False,
+                        "name": enums.ETLTransformer.SUMMARIZE.value,
+                        "system_prompt": None,
+                        "user_prompt": None,
+                    },
+                ]
+            },
+            load_config={
+                "refinery_project": {"enabled": False, "id": None},
+                "markdown_file": {"enabled": True, "id": str(markdown_file.id)},
+            },
+            notify_config={
+                "http": {
+                    "url": "http://cognition-gateway:80/etl/finished/{markdown_file_id}",
+                    "format": {
+                        "markdown_file_id": str(markdown_file.id),
+                    },
+                    "method": "POST",
+                }
+            },
             priority=priority,
         )
 
