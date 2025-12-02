b2074944ca8a322e5f0601de62d53baac0f669e0
Andrea Hrelja <151508315+andhreljaKern@users.noreply.github.com>
2025-12-02 02:06:31 +0100

    Cognition ETL Provider (#361)
    * chore: update submodules
    
    * perf(alembic): add etl task table
    
    * chore: update submodules
    
    * perf(alembic): update etl task table
    
    * chore: update submodules
    
    * perf(alembic): add org_id column
    
    * chore: update submodules
    
    * perf(alembic): update etl task table
    
    * chore: update submodules
    
    * perf: add file_size_bytes to etl_task
    
    * chore: update submodules
    
    * perf: add split_config
    
    * chore: update submodules
    
    * perf(etl): fkey alignment
    
    * chore: update submodules
    
    * perf: task cancellation
    
    * chore: update submodules
    
    * fix: update submodules merge conflict
    
    * perf: add cache_config
    
    * perf: align /notify to etl provider
    
    * chore: update submodules
    
    * chore: update submodules
    
    * perf: update minio_upload for execute_etl
    
    * fix: markdown_file update after etl_task creation
    
    * chore: merge dev
    
    * perf: add etl task table
    
    * perf: disable CLEANSE as default
    
    * chore: update submodules
    
    * perf: standard cache config keys
    
    * Merge with dev
    
    * Alembic new table & submodule fix import
    
    * tmp commit
    
    * Tmp doc almost
    
    * No cache tmp doc working
    
    * Submodule update
    
    * Ensure complete
    
    * chore: update submodules
    
    * perf(alembic): etl original file name
    
    * chore: update submodules
    
    * perf(alembic): update etl content
    
    * perf(alembic): full_admin table
    
    * chore: update submodules
    
    * perf: tmp doc etl task metadata
    
    * Conversion method + alembic
    
    * Hotfix default value
    
    * tmp doc changes
    
    * perf: minio upload minor enhancement
    
    * chore: update submodules
    
    * submodule change
    
    * chore: update submodules
    
    * chore: update submodules
    
    * perf(alembic): new fields
    
    * chore: update submodules
    
    * fix: alembic merge conflict
    
    ---------
    
    Co-authored-by: JWittmeyer <jens.wittmeyer@kern.ai>

--- controller/transfer/cognition/minio_upload.py
+++ controller/transfer/cognition/minio_upload.py
@@ -1,54 +1,125 @@
 from typing import List
-from submodules.model.cognition_objects import file_reference as file_reference_db_bo
-from submodules.model.enums import TaskType, FileCachingProcessingScope
+
 from controller.task_master import manager as task_master_manager
-from submodules.model import enums
+from submodules.model import enums, etl_utils
 from submodules.model.business_objects import general
+from submodules.model.global_objects import etl_task as etl_task_bo
+from submodules.model.cognition_objects import (
+    file_reference as file_reference_db_bo,
+    markdown_file as markdown_file_bo,
+    markdown_dataset as markdown_dataset_bo,
+)
 
 
 def handle_cognition_file_upload(path_parts: List[str]):
-
+    # raise NotImplementedError("This function is not yet implemented.")
     if path_parts[1] != "_cognition" or len(path_parts) < 5:
         return
-    if path_parts[2] == "files" and path_parts[4].startswith("file_original"):
-        org_id = path_parts[0]
-        file_hash, file_size = path_parts[3].split("_")
-        file_reference = file_reference_db_bo.get(org_id, file_hash, int(file_size))
+    ##tmp doc retrieval => need to understand how .info file is an indicator for cognition gateway to pick it up
+    if not (path_parts[2] == "files" and path_parts[4].startswith("file_original")):
+        return
 
-        if (
-            not file_reference
-            or file_reference.state == enums.FileCachingState.RUNNING.value
-            or file_reference.state == enums.FileCachingState.COMPLETED.value
-        ):
-            # file_reference is None or already processed in queue
-            print("File reference duplication error, file is already processed", flush=True)
-            if file_reference:
-                print(f"File reference id: {str(file_reference.id)}", flush=True)
-                print(f"File name: {file_reference.original_file_name}", flush=True)
-            return
-        file_reference.state = enums.FileCachingState.COMPLETED.value
-        general.commit()
+    org_id = path_parts[0]
+    file_hash, file_size = path_parts[3].split("_")
+    file_reference = file_reference_db_bo.get(org_id, file_hash, int(file_size))
 
-        prio = (
-            file_reference.meta_data.get("transformation_initiator")
-            == enums.FileCachingInitiator.TMP_DOC_RETRIEVAL.value
+    if (
+        not file_reference
+        or file_reference.state == enums.FileCachingState.RUNNING.value
+        or file_reference.state == enums.FileCachingState.COMPLETED.value
+    ):
+        # file_reference is None or already processed in queue
+        print(
+            f"WARNING:  {__name__} - file reference duplication error, file is already processed",
+            flush=True,
         )
-        extraction_method = file_reference.meta_data.get("extraction_method")
+        if file_reference:
+            print(
+                f"INFO:     {__name__} - file reference id: {str(file_reference.id)}",
+                flush=True,
+            )
+            print(
+                f"INFO:     {__name__} - file name: {file_reference.original_file_name}",
+                flush=True,
+            )
+        return
+
+    file_reference.state = enums.FileCachingState.COMPLETED.value
+    general.commit()
+
+    if (
+        file_reference.meta_data.get("file_caching_initiator")
+        == enums.FileCachingInitiator.TMP_DOC_RETRIEVAL.value
+    ):
+        project_id = file_reference.meta_data.get("project_id")
+        conversation_id = file_reference.meta_data.get("conversation_id")
+        full_config, tokenizer = etl_utils.get_full_config_and_tokenizer_from_config_id(
+            file_reference, project_id=project_id, conversation_id=conversation_id
+        )
+        etl_task = etl_task_bo.create(
+            org_id,
+            file_reference.created_by,
+            file_reference.original_file_name,
+            file_reference.file_size_bytes,
+            full_config=full_config,
+            tokenizer=tokenizer,
+            meta_data={
+                "file_reference_id": str(file_reference.id),
+                "tmp_doc_metadata": {
+                    "project_id": project_id,
+                    "conversation_id": conversation_id,
+                },
+            },
+            priority=1,
+        )
 
         task_master_manager.queue_task(
-            str(file_reference.organization_id),
+            org_id,
             str(file_reference.created_by),
-            TaskType.PARSE_COGNITION_FILE,
+            enums.TaskType.EXECUTE_ETL,
             {
-                "parse_scope": FileCachingProcessingScope.EXTRACT_TRANSFORM.value,
-                "file_reference_id": str(file_reference.id),
-                "extraction_method": extraction_method,
-                "meta_data": file_reference.meta_data,
-                "extraction_key": file_reference.meta_data.get("extraction_key"),
-                "transformation_key": file_reference.meta_data.get(
-                    "transformation_key"
-                ),
-                "file_name": file_reference.original_file_name,
+                "etl_task_id": str(etl_task.id),
+                **etl_task.meta_data,
             },
-            prio,  # not sure if prio is right here as the prio tasks should only take < 1 min but waiting for the normal queue will take ages depending on the queue
+            priority=True,
+        )
+
+    else:
+        priority = -1
+
+        markdown_file = markdown_file_bo.get(
+            org_id, file_reference.meta_data.get("markdown_file_id")
+        )
+
+        full_config, tokenizer = etl_utils.get_full_config_and_tokenizer_from_config_id(
+            file_reference
+        )
+        etl_task = etl_task_bo.create(
+            org_id,
+            file_reference.created_by,
+            file_reference.original_file_name,
+            file_reference.file_size_bytes,
+            full_config=full_config,
+            tokenizer=tokenizer,
+            meta_data={
+                "file_reference_id": str(file_reference.id),
+            },
+            priority=priority,
+        )
+
+        markdown_file_bo.update(
+            org_id=org_id,
+            markdown_file_id=markdown_file.id,
+            etl_task_id=etl_task.id,
+        )
+
+        task_master_manager.queue_task(
+            org_id,
+            str(file_reference.created_by),
+            enums.TaskType.EXECUTE_ETL,
+            {
+                "etl_task_id": str(etl_task.id),
+                **etl_task.meta_data,
+            },
+            priority=priority != -1,
         )
