06b69eabcce2df0ee99ae826750b82204412ed9d
JWittmeyer <91723236+JWittmeyer@users.noreply.github.com>
2026-02-23 15:12:08 +0100

    Admin check fix (#390)
    * Admin check fix
    
    * More admin routes unified
    
    * fix admin check
    
    * Submodule change

--- middleware/database_session.py
+++ middleware/database_session.py
@@ -8,11 +8,13 @@ from middleware import log_storage
 from fast_api.routes.client_response import GENERIC_FAILURE_RESPONSE
 import traceback
 from submodules.model.session_wrapper import run_db_async_with_session
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
 async def handle_db_session(request: Request, call_next):
+    auth_manager.parse_admin_info(request)
     info = _prepare_info(request)
     general.get_ctx_token()
     try:
@@ -20,7 +22,9 @@ async def handle_db_session(request: Request, call_next):
         request.state.info = info
         request.state.parsed = {}
 
-        log_request = await run_db_async_with_session(auth_manager.extract_state_info, request, "log_request")
+        log_request = await run_db_async_with_session(
+            auth_manager.extract_state_info, request, "log_request"
+        )
         length = request.headers.get("content-length")
 
         if length and int(length) > 0:
