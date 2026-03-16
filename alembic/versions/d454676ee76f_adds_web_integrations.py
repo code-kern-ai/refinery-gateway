91e39017a6618dfe06418c2730ff4bb205975b0e
LennartSchmidtKern <lennart.schmidt@kern.ai>
2026-03-16 15:59:47 +0100

    fix: alembic merge

--- alembic/versions/d454676ee76f_adds_web_integrations.py
+++ alembic/versions/d454676ee76f_adds_web_integrations.py
@@ -1,7 +1,7 @@
 """adds web integrations
 
 Revision ID: d454676ee76f
-Revises: e08f38f06e98
+Revises: 58585c688391
 Create Date: 2026-03-03 16:51:11.127269
 
 """
@@ -12,7 +12,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision = "d454676ee76f"
-down_revision = "e08f38f06e98"
+down_revision = "58585c688391"
 branch_labels = None
 depends_on = None
 
